c03d015d5a4ca1250e4230b3ad3b0d50ffa20817
AbrahamZambrano <abrahancillo83@gmail.com>
2022-11-04 17:17:25 +0100

    fixing buttons

--- prueba.py
+++ prueba.py
@@ -4,31 +4,38 @@ from textual.widgets import Button, Header, Footer, Static
 import re
 import subprocess
 
-def get_namespaces():
+def get_namespaces() -> list:
     # Ejecutar comando de kubernetes para sacar el texto
     # Guardar el resultado en un array 
 
+    #ejecutas el comando y obtienes el resultado en utf-8
     namespaces = subprocess.run(["kubectl", "get", "namespaces"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    
+    #Parsea el resultado
+    namespaces = re.sub("\s*[0-9]h", " ", namespaces)
     namespaces = namespaces.replace("\n", " ")
-    namespaces = namespaces.replace("24h", " ")
     namespaces = namespaces.replace("Active", " ")
+
+    #Encuentra unicamente los nombres de los contenedores
     res = re.findall(r"[a-z0-9](?!.*--)[a-z0-9-]{1,61}[a-z0-9]", namespaces)
     
     return res
 
 
-class TimeDisplay(Static):
-    """A widget to display elapsed time."""
+def pods(contenedor: str) -> str:
+    pods = subprocess.run(["kubectl", "get", "pods", "-n", contenedor], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    return pods
 
-class Stopwatch(Static):
+
+class NamespacesWidget(Static):
     """A stopwatch widget."""
-
     def compose(self) -> ComposeResult:
         """Create child widgets of a stopwatch."""
         for namespace in get_namespaces():
-            yield Button(namespace)
+            yield Button(namespace, id=namespace)
 
-class StopwatchApp(App):
+
+class KUIAppUI(App):
     """A Textual app to manage stopwatches."""
 
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
@@ -37,16 +44,22 @@ class StopwatchApp(App):
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
-        yield Container(Stopwatch())
+        yield Container(NamespacesWidget())
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
+    
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Event handler called when a button is pressed."""
+        container = event.button.id
+        pod_info = pods(container)
+
+
 
 
 if __name__ == "__main__":
-    app = StopwatchApp()
-    
-    namespace = Stopwatch()
+    app = KUIAppUI()
+    namespace = NamespacesWidget()
     app.run()
     
